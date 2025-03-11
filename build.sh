cad257e166fc0bfbe7c526dee43dbf659bd00bea
CatBot-Crying <88854387+CatBot-Crying@users.noreply.github.com>
2025-03-11 11:55:22 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://dl.google.com/android/repository/android-ndk-r27c-linux.zip
-unzip android-ndk-r27c-linux.zip
-./android-ndk-r28/build/ndk-build
+unzip android-ndk-r26-linux.zip
+./android-ndk-r26/build/ndk-build
