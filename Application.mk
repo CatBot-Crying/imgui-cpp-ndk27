e6358ea21532cab16730efbfe1768dba5fa7425a
CatBot-Crying <88854387+CatBot-Crying@users.noreply.github.com>
2025-03-11 14:09:13 +0700

    Update Application.mk

--- Application.mk
+++ Application.mk
@@ -1,6 +1,7 @@
 APP_STL := c++_static
-APP_ABI := arm64-v8a
+APP_ABI := armeabi-v7a arm64-v8a
 APP_PLATFORM := android-21
 APP_OPTIM := release
 APP_THIN_ARCHIVE := true
 APP_PIE 		:= true
+NDK_TOOLCHAIN_VERSION := clang
